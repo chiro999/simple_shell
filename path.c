f9a91cefa0dcd4c6c2eec53c819af150bca04a31
chiro999 <r.o.g.u.e@outlook.com>
2023-08-23 16:10:10 -0700

    100

--- path.c
+++ path.c
@@ -13,14 +13,19 @@ int _execute(char *command, shell_t *shell_vars)
 
 	if (access(command, X_OK) == 0)
 	{
+		/* create child process using fork */
 		child = fork();
+
+		/* if it is negative an error occured */
 		if (child == -1)
 			print_error(shell_vars, NULL);
+		/*if the child process occured 0 */
 		if (child == 0)
 		{
 			if (execve(command, shell_vars->tokens, shell_vars->env_vars) == -1)
 				print_error(shell_vars, NULL);
 		}
+		/* if it is the parent process that is running */
 		else
 		{
 			wait(&shell_vars->status);
@@ -33,6 +38,7 @@ int _execute(char *command, shell_t *shell_vars)
 		shell_vars->status = 127;
 		return (1);
 	}
+	/* if user lacks permission to execute command */
 	else
 	{
 		print_error(shell_vars, ": Permission denied\n");
@@ -42,16 +48,17 @@ int _execute(char *command, shell_t *shell_vars)
 }
 
 /**
- * str_path - finds the PATH variable
- * @env: array of environment variables
+ * str_path - find the PATH variable
+ * @env_arr:  environment variables
  *
- * Return: pointer to the node that contains the PATH, or NULL on failure
+ * Return: the node that contains the PATH, or NULL on failure
  */
 char *str_path(char **env_arr)
 {
 	char *path = "PATH=";
 	unsigned int i, j;
 
+	/* for loop to check which env_arr has 5 characters and is equal to 'PATH='*/
 	for (i = 0; env_arr[i] != NULL; i++)
 	{
 		for (j = 0; j < 5; j++)
@@ -65,21 +72,23 @@ char *str_path(char **env_arr)
 }
 
 /**
- * path_check - checks if the command is in the PATH
- * @inputs: variables
+ * path_check - check if  the command resides in the PATH
+ * @shell_vars: variables
  *
  * Return: void
  */
 void path_check(shell_t *shell_vars)
 {
 	char *path, *dup = NULL, *check = NULL;
-	unsigned int i = 0, r = 0;
+	unsigned int i = 0, exec_check = 0;
 	char **path_tokens;
-
+	
+	/* is command in the current directory */
 	if (is_PATH(shell_vars->tokens[0]))
-		r = exec_curr_dir(shell_vars);
+		exec_check = exec_curr_dir(shell_vars);
 	else
-	{
+	{	
+		/* find PATH from env variables */
 		path = str_path(shell_vars->env_vars);
 		if (path != NULL)
 		{
@@ -88,20 +97,24 @@ void path_check(shell_t *shell_vars)
 			for (i = 0; path_tokens && path_tokens[i]; i++, free(check))
 			{
 				check = _strcat(path_tokens[i], shell_vars->tokens[0]);
+				/* does command file exist */
 				if (access(check, F_OK) == 0)
 				{
-					r = _execute(check, shell_vars);
+					exec_check = _execute(check, shell_vars);
 					free(check);
 					break;
 				}
 			}
+			/* free memory */
 			free(dup);
+			/* if no paths */
 			if (path_tokens == NULL)
 			{
 				shell_vars->status = 127;
 				_close(shell_vars);
 			}
 		}
+		/* if the command is not in any path- print error message */
 		if (path == NULL || path_tokens[i] == NULL)
 		{
 			print_error(shell_vars, ": not found\n");
@@ -109,34 +122,43 @@ void path_check(shell_t *shell_vars)
 		}
 		free(path_tokens);
 	}
-	if (r == 1)
+	/* close if execution is succesful */
+	if (exec_check == 1)
 		_close(shell_vars);
 }
 
 /**
- * exec_curr_dir - executes the command in the current working directory
- * @shell_vars: pointer to struct of variables
+ * exec_curr_dir - executes command in the current  directory
+ * @shell_vars: struct of variables
  *
  * Return: 0 on success, 1 on failure
  */
 int exec_curr_dir(shell_t *shell_vars)
 {
 	pid_t child;
-
+	
+	/* does command file exist */
 	if (access(shell_vars->tokens[0], F_OK) == 0)
-	{
+	{	
+		/* is command file executable */
 		if (access(shell_vars->tokens[0], X_OK) == 0)
-		{
+		{	
+			/* create child process using fork */
 			child = fork();
+			/* failed to create child process */
 			if (child == -1)
 				print_error(shell_vars, NULL);
+			/* child process */
 			if (child == 0)
-			{
+			{	
+				/* check for execution failure */
 				if (execve(shell_vars->tokens[0], shell_vars->tokens, shell_vars->env_vars) == -1)
 					print_error(shell_vars, NULL);
 			}
+			/* parent process */
 			else
 			{
+				/* wait for the child process to complete and check the exit status of child process */
 				wait(&shell_vars->status);
 				if (WIFEXITED(shell_vars->status))
 					shell_vars->status = WEXITSTATUS(shell_vars->status);
@@ -144,6 +166,7 @@ int exec_curr_dir(shell_t *shell_vars)
 					shell_vars->status = 130;
 				return (0);
 			}
+			/* fork failed */
 			shell_vars->status = 127;
 			return (1);
 		}
@@ -168,7 +191,7 @@ int exec_curr_dir(shell_t *shell_vars)
 int is_PATH(char *name)
 {
 	unsigned int i;
-
+	/* search for path separator '/' */
 	for (i = 0; name[i]; i++)
 	{
 		if (name[i] == '/')
