84cbdf3053e1ee7e1a148fc576fca20ef1f43b1e
chiro999 <r.o.g.u.e@outlook.com>
2023-08-23 17:16:12 -0700

    strtok

--- strtok.c
+++ strtok.c
@@ -1,17 +1,17 @@
 #include "shell.h"
 
 /**
- * is_a_match - checks if a character matches any in a string
- * @c: character to check
- * @str: string to check
+ * is_a_match - checks for character matches  in a string
+ * @c: character to be checked
+ * @str: string to checked
  *
- * Return: 1 if match, 0 if not
+ * Return: 1 if identical else 0
  */
 unsigned int is_a_match(char c, const char *str)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (; str[i] != '\0'; i++)
 	{
 		if (c == str[i])
 			return (1);
@@ -22,46 +22,55 @@ unsigned int is_a_match(char c, const char *str)
 /**
  * custom_strtok - custom strtok
  * @str: string to tokenize
- * @delim: delimiter to tokenize against
+ * @delim: character to be used as delimiter
  *
  * Return: pointer to the next token or NULL
  */
 char *custom_strtok(char *str, const char *delim)
-{
+{	
+	/* token represent the current token */
 	static char *token;
-	static char *next;
-	unsigned int i;
+	static char *next_token;
+	unsigned int i = 0;
 
-	if (str != NULL)
-		next = str;
-	token = next;
-	if (token == NULL)
+	if (str)
+		next_token = str;
+	token = next_token;
+	if (!token)
 		return (NULL);
-	for (i = 0; next[i] != '\0'; i++)
+
+	/* find the first non-delim char */
+	for (; next_token[i] != '\0'; i++)
 	{
-		if (is_a_match(next[i], delim) == 0)
+		if (is_a_match(next_token[i], delim) == 0)
 			break;
 	}
-	if (next[i] == '\0' || next[i] == '#')
+	/* search for null terminitor or '#' */
+	if (next_token[i] == '\0' || next_token[i] == '#')
 	{
-		next = NULL;
+		next_token = NULL;
 		return (NULL);
 	}
-	token = next + i;
-	next = token;
-	for (i = 0; next[i] != '\0'; i++)
+	/* set token to non-delim char (will be useful in next for-loop */
+	token = next_token + i;
+	next_token = token;
+
+	/* find the next delim char */
+	for (i = 0; next_token[i] != '\0'; i++)
 	{
-		if (is_a_match(next[i], delim) == 1)
+		if (is_a_match(next_token[i], delim) == 1)
 			break;
 	}
-	if (next[i] == '\0')
-		next = NULL;
+	if (next_token[i] == '\0')
+		next_token = NULL;
 	else
-	{
-		next[i] = '\0';
-		next = next + i + 1;
-		if (*next == '\0')
-			next = NULL;
+	{	
+		/* replace delim char with '\0' */
+		next_token[i] = '\0';
+		next_token +=  i + 1;
+		/* set next token to NULL if we are at the end of the string */
+		if (*next_token == '\0')
+			next_token = NULL;
 	}
 	return (token);
 }
