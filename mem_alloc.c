65b3d1d6fee67ce1cd0788f0e9aa3dc2b573e2e8
chiro999 <r.o.g.u.e@outlook.com>
2023-08-23 17:20:01 -0700

    mem

--- mem_alloc.c
+++ mem_alloc.c
@@ -1,28 +1,28 @@
 #include "shell.h"
 
 /**
- * more_mem - reallocates a pointer to double the space
- * @ptr: pointer to the old array
- * @size: pointer to number of elements in the old array
+ * more_mem - doubles or increases the memory space
+ * @old_arr: old array
+ * @size: number of elements in old_array
  *
  * Return: pointer to the new array
  */
 char **more_mem(char **old_arr, size_t *size)
 {
-	char **new;
+	char **new_arr;
 	size_t i;
 
-	new = malloc(sizeof(char *) * ((*size) + 10));
-	if (new == NULL)
+	new_arr = malloc(sizeof(char *) * ((*size) + 10));
+	if (new_arr == NULL)
 	{
 		free(old_arr);
 		return (NULL);
 	}
 	for (i = 0; i < (*size); i++)
 	{
-		new[i] = old_arr[i];
+		new_arr[i] = old_arr[i];
 	}
 	*size += 10;
 	free(old_arr);
-	return (new);
+	return (new_arr);
 }
