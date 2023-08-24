48da3c0f3ca5b48a80cd84c077593c2e2387a49f
chiro999 <r.o.g.u.e@outlook.com>
2023-08-23 17:24:45 -0700

    100

--- mem_alloc.c
+++ mem_alloc.c
@@ -3,22 +3,24 @@
 /**
  * more_mem - doubles or increases the memory space
  * @old_arr: old array
- * @size: number of elements in old_array
+ * @n: number of elements in old_array
  *
  * Return: pointer to the new array
  */
-char **more_mem(char **old_arr, size_t *size)
+char **more_mem(char **old_arr, size_t *n)
 {
 	char **new_arr;
-	size_t i;
+	size_t i = 0;
 
-	new_arr = malloc(sizeof(char *) * ((*size) + 10));
+	/* new array with increased memory */
+	new_arr = malloc(sizeof(char *) * ((*n) + 10));
 	if (new_arr == NULL)
 	{
 		free(old_arr);
 		return (NULL);
 	}
-	for (i = 0; i < (*size); i++)
+	/* transfer elements from old arr to new arr iteratively */
+	for (; i < (*n); i++)
 	{
 		new_arr[i] = old_arr[i];
 	}
