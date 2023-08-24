5396fe0dfcff2d8cd287d56fd4cc972d4b30ad83
chiro999 <r.o.g.u.e@outlook.com>
2023-08-23 17:00:14 -0700

    100

--- exit.c
+++ exit.c
@@ -7,18 +7,23 @@
  */
 void _close(shell_t *shell_vars)
 {
+	/* initialize status */
 	int status;
-
+	
+	/* check if command matches "exit" on the selector and status is avaiable */
 	if (_strcmp(shell_vars->tokens[0], "exit") == 0 &&
-	    shell_vars->tokens[1] != NULL)
+	    shell_vars->tokens[1])
 	{
+		/* convert status to an integer */
 		status = _atoi(shell_vars->tokens[1]);
 		if (status == -1)
 		{
+			/* set status as 2 as illegal input */
 			shell_vars->status = 2;
 			print_error(shell_vars, ": Illegal number: ");
 			str_error(shell_vars->tokens[1]);
 			str_error("\n");
+
 			free(shell_vars->commands);
 			shell_vars->commands = NULL;
 			return;
